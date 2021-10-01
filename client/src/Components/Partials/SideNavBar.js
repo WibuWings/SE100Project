04161cf1a93f77af47ea1bbd34bd84b8f10af779
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-10-02 01:04:59 +0700

    Add sidebar header footer

--- client/src/Components/Partials/SideNavBar.js
+++ client/src/Components/Partials/SideNavBar.js
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import '../../CSS/DashBoard.css'
 import { 
     AiOutlineDashboard,
+    AiFillDashboard,
     AiOutlineContainer
 } from "react-icons/ai";
 import { ImProfile } from "react-icons/im";
-import { BsFillPeopleFill } from "react-icons/bs";
+import { BsFillPeopleFill,BsInfoCircleFill} from "react-icons/bs";
 import { 
     IoPeopleOutline,
     IoReceiptOutline
 } from "react-icons/io5";
+import { FaSignOutAlt } from "react-icons/fa";
 
+import Kurisu from '../../img/kurisu.jpg';
 
 class SideNavBar extends Component {
     render() {
         return (
             <div>
-                <div class="navbar sidebar">
-                    <a href="#" class="nav-item activate">
-                        <AiOutlineDashboard class="nav-item-icon"/>   
+                <div class="navibar sidebar">
+                    <div class="nav-heading">
+                        <img src={Kurisu} style ={{width: 60, height: 60, borderRadius: '100%'}}></img>
+                        <span class="user-name" style ={{marginLeft: 10, color: '#fff'}}>Makise Kurisu</span>
+                    </div>
+                    <a href="#" class="nav-item">
+                        <AiOutlineDashboard class="nav-item-icon"/>
+                        <AiFillDashboard class="nav-item-icon icon-activate"/>   
                         <span>Dashboard</span>
                     </a>
                     <a href="#" class="nav-item">
@@ -37,6 +45,16 @@ class SideNavBar extends Component {
                         <IoReceiptOutline class="nav-item-icon"/>
                         <span>Receipt Manager</span>
                     </a>
+                    <div class="nav-footer">
+                        <a href="#" class="nav-item">
+                            <FaSignOutAlt class="nav-item-icon"/>
+                            <span>Sign out</span>
+                        </a>
+                        <a href="#" class="nav-item">
+                            <BsInfoCircleFill class="nav-item-icon"/>
+                            <span>About</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         );
