6a4183a495b006c9635d60d101b13d38029142fd
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-10-02 10:04:14 +0700

    Chỉnh sửa hiệu ứng navbar

--- client/src/Components/Partials/SideNavBar.js
+++ client/src/Components/Partials/SideNavBar.js
@@ -3,14 +3,19 @@ import '../../CSS/DashBoard.css'
 import { 
     AiOutlineDashboard,
     AiFillDashboard,
-    AiOutlineContainer
+    AiOutlineContainer,
+    AiFillContainer
 } from "react-icons/ai";
 import { ImProfile } from "react-icons/im";
 import { BsFillPeopleFill,BsInfoCircleFill} from "react-icons/bs";
 import { 
     IoPeopleOutline,
-    IoReceiptOutline
+    IoPeopleSharp,
+    IoReceiptOutline,
+    IoReceiptSharp
 } from "react-icons/io5";
+import { RiProfileFill, RiProfileLine } from "react-icons/ri";
+
 import { FaSignOutAlt } from "react-icons/fa";
 
 import Kurisu from '../../img/kurisu.jpg';
@@ -21,36 +26,43 @@ class SideNavBar extends Component {
             <div>
                 <div class="navibar sidebar">
                     <div class="nav-heading">
-                        <img src={Kurisu} style ={{width: 60, height: 60, borderRadius: '100%'}}></img>
+                        <img src={Kurisu} style ={{width: 50, height: 50, borderRadius: '100%'}}></img>
                         <span class="user-name" style ={{marginLeft: 10, color: '#fff'}}>Makise Kurisu</span>
                     </div>
-                    <a href="#" class="nav-item">
-                        <AiOutlineDashboard class="nav-item-icon"/>
-                        <AiFillDashboard class="nav-item-icon icon-activate"/>   
-                        <span>Dashboard</span>
-                    </a>
-                    <a href="#" class="nav-item">
-                        <ImProfile class="nav-item-icon"/>
-                        <span>Profile</span>
-                    </a>
-                    <a href="#" class="nav-item">
-                        <IoPeopleOutline class="nav-item-icon"/>
-                        <span>Employee Manager</span>
-                    </a>
-                    <a href="#" class="nav-item">
-                        <AiOutlineContainer class="nav-item-icon"/>
-                        <span>Goods Manager</span>
-                    </a>
-                    <a href="#" class="nav-item">
-                        <IoReceiptOutline class="nav-item-icon"/>
-                        <span>Receipt Manager</span>
-                    </a>
-                    <div class="nav-footer">
+                    <div class="nav-container">
                         <a href="#" class="nav-item">
+                            <AiOutlineDashboard class="nav-item-icon"/>
+                            <AiFillDashboard class="nav-item-icon icon-activate"/>   
+                            <span>Dashboard</span>
+                        </a>
+                        <a href="#" class="nav-item">
+                            <RiProfileLine class="nav-item-icon"/>
+                            <RiProfileFill class="nav-item-icon icon-activate"/>
+                            <span>Profile</span>
+                        </a>
+                        <a href="#" class="nav-item">
+                            <IoPeopleOutline class="nav-item-icon"/>
+                            <IoPeopleSharp class="nav-item-icon icon-activate"/>
+                            <span>Employee Manager</span>
+                        </a>
+                        <a href="#" class="nav-item">
+                            <AiOutlineContainer class="nav-item-icon"/>
+                            <AiFillContainer class="nav-item-icon icon-activate"/>
+                            <span>Goods Manager</span>
+                        </a>
+                        <a href="#" class="nav-item">
+                            <IoReceiptOutline class="nav-item-icon"/>
+                            <IoReceiptSharp class="nav-item-icon icon-activate"/>
+                            <span>Receipt Manager</span>
+                        </a>
+                    </div>
+                    
+                    <div class="nav-footer">
+                        <a href="#" class="nav-item" style={{flex: 5}}>
                             <FaSignOutAlt class="nav-item-icon"/>
                             <span>Sign out</span>
                         </a>
-                        <a href="#" class="nav-item">
+                        <a href="#" class="nav-item" style={{flex: 4}}>
                             <BsInfoCircleFill class="nav-item-icon"/>
                             <span>About</span>
                         </a>
