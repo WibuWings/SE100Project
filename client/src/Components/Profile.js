69b1caef8a7bee0b9ea4dd508f6f073a9cc9aa82
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2021-10-07 16:29:02 +0700

    hello

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ProfileHeader from './Profile/ProfileHeader';
 import '../CSS/Profile.css';
+import { Container, Grid } from '@mui/material';
+import ProfileDetail from './Profile/ProfileDetail';
+import ProfileAvt from './Profile/ProfileAvt';
 
 
 class Profile extends Component {
@@ -8,6 +11,17 @@ class Profile extends Component {
         return (
             <div className="profile" >
                 <ProfileHeader></ProfileHeader>
+                <Container maxWidth="xl">
+                    <Grid className="profile-body" container  spacing={2}>
+                        <Grid item xs={8} >
+                            <ProfileDetail></ProfileDetail>
+                        </Grid>
+                        <Grid item xs={4} >
+                            <ProfileAvt></ProfileAvt>
+                        </Grid>
+                    </Grid>
+                </Container>
+
             </div>
         );
     }
--- client/src/Components/Profile/ProfileHeader.js
+++ client/src/Components/Profile/ProfileHeader.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import avatarImg from '../../img/kurisu.jpg'
 import Avatar from '@mui/material/Avatar';
-import Divider from '@mui/material/Divider'
-import { BiEditAlt, BiReceipt } from "react-icons/bi";
-
+import Divider from '@mui/material/Divider';
+import { BiEditAlt } from "react-icons/bi";
+import { FaTelegramPlane } from "react-icons/fa";
 class ProfileHeader extends Component {
     render() {
         return (
-            <div className="profile-header" style={{ width: '100%', height: '350px' }}>
+            <div className="profile-header" style={{ width: '100%', height: '300px' }}>
                 <Avatar
                     className="profile-header__avt"
                     src={avatarImg}
                     sx={{ width: 100, height: 100, border: '5px solid #9999996b', }}
                 ></Avatar>
-                <h2 className="profile-header__name">
-                    Thiện Phước
-                </h2>
+                
                 <div className="profile-header-des">
-                    <p className="profile-header__description">Description about me!</p>
+                    <h2 className="profile-header__name">
+                        Thiện Phước
+                    </h2>
                     <BiEditAlt className="profile-header__icon"></BiEditAlt>
+                </div>
+                <div style={{display: 'flex', fontSize:'1rem', color:"white"}}>
+                    <FaTelegramPlane className="profile-header__icon-tel"></FaTelegramPlane>
+                    <p className="profile-header__tel">0387527010</p>
                 </div>
                 <Divider className="profile-header__divider"></Divider>
             </div>
