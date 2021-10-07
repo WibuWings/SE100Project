bc8e533ff91237f412a169acb61995a5cb2a8fde
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-10-07 16:51:36 +0700

    Update auth.js

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,10 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth');
-
+//--------
+//these router handle sign in and sign up
 router.post("/sign-in-with-gmail-password", authController.authSignInRegular);
 router.post("/sign-in-with-google", authController.authSignInWithGG);
 router.post("/find-password", authController.forgetPassword);
 router.post("/register-with-email", authController.register);
+//
 
+//--------
+//these router handle authentication and authorization
+
+//
 module.exports = router;
