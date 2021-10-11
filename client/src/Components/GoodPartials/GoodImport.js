1e97be8745065961a65c1a57efebe274137526db
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-10-12 00:22:56 +0700

    edit base for good import

--- client/src/Components/GoodPartials/GoodImport.js
+++ client/src/Components/GoodPartials/GoodImport.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { TextField } from '@mui/material';
 
 class GoodImport extends Component {
     render() {
         return(
             <div>
-                Good Import
-                
+                <div>Good Import</div>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             </div>
         );        
     }
