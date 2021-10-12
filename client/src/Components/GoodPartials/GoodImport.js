60380007d3f96269933e66d64863b88d305b9767
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-10-12 16:54:04 +0700

    edit and get data in good import

--- client/src/Components/GoodPartials/GoodImport.js
+++ client/src/Components/GoodPartials/GoodImport.js
@@ -3,7 +3,7 @@ import { TextField } from '@mui/material';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import { InputLabel, Button } from '@mui/material';
+import { InputLabel, Button, Modal } from '@mui/material';
 
 const productTypes =[
      'food', 'detergent', 'cuisine'
@@ -13,44 +13,73 @@ class GoodImport extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          type: 'none', 
+          good: {
+            id: '',
+            name: '',
+            quantity: '',
+            originPrice: 0,
+            sellPrice: 0,
+            importTime: '',
+            expiredDate: '',
+            type: 'none',
+          },
+          show: false,
         };
-      }
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+    }
+    showModal = () => {
+        this.setState({ show: true });
+      };
+    
+    hideModal = () => {
+        this.setState({ show: false });
+    };
     btnClick () {
-        alert('Fuck');
+        alert('Chịu');
     }  
     render() {
         return(
             <div>
                 <div>Good Import</div>
+                <Button
+                    variant="contained"
+                    component="label"
+                    >
+                    Upload File
+                    <input
+                        type="file"
+                        hidden
+                    />
+                </Button>
                 <div class="info-container">
                     <div class="id-displaying">
                         <div class="input-label">ID</div>
-                        <TextField type= "text" class="input-val"size="small" ref="goodId" variant="outlined" defaultValue='1212121' inputProps={{ readOnly: true}}/>
+                        <TextField type= "text" class="input-val"size="small" value={this.state.good.id} variant="outlined" defaultValue='1212121' inputProps={{ readOnly: true}}/>
                     </div>
                     <div class="input-container">
                         <div class="input-label">Name</div>
-                        <TextField type="text" class="input-val" size="small" ref="good-name" variant="outlined" />
+                        <TextField type="text" class="input-val" size="small"  value={this.state.good.name} variant="outlined" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Quantity:</div>
-                        <TextField type="number" class="input-val" size="small" ref="good-quantity" variant="outlined" />
+                        <TextField type="number" class="input-val" size="small" value={this.state.good.quantity} variant="outlined" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Original Price:</div>
-                        <TextField type="number" class="input-val" size="small" ref="good-original-price" variant="outlined" />
+                        <TextField type="number" class="input-val" size="small" value={this.state.good.originPrice} variant="outlined" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Sell Price:</div>
-                        <TextField type="number" class="input-val"size="small" ref="good-sell-price" variant="outlined" />
+                        <TextField type="number" class="input-val"size="small" value={this.state.good.sellPrice} variant="outlined" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Import Time:</div>
-                        <TextField type="date" class="input-val" size="small" ref="good-import-time" variant="outlined" defaultValue="2017-05-24" />
+                        <TextField type="date" class="input-val" size="small" value={this.state.good.importTime} variant="outlined" defaultValue="2017-05-24" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Expired Date:</div>
-                        <TextField type="date" class="input-val" size="small" ref="good-expired-date" variant="outlined" />
+                        <TextField type="date" class="input-val" size="small" value={this.state.good.expiredDate} variant="outlined" />
                     </div>
                     <div class="input-container">
                         <div class="input-label">Product Type:</div>
@@ -59,7 +88,7 @@ class GoodImport extends Component {
                             <Select
                                 labelid="select-filled-label"
                                 id="select-filled"
-                                value={this.state.type}
+                                value={this.state.good.type}
                                 onChange={(event) => {
                                     this.setState({type: event.target.value});
                                 }}
@@ -72,9 +101,14 @@ class GoodImport extends Component {
                                 }   
                             </Select>
                         </FormControl>
-                        <Button variant="text">
-                            Add Type
-                        </Button>
+                        <Button onClick={this.showModal}>Add type</Button>
+                        {/* <Modal
+                            open={this.state.show}
+                            onClose={this.hideModal}
+                        >
+                            Nguyễn Công Phi
+                        </Modal> */}
+                        {/* Chỗ này lỗi mẹ rồi =))) */}
                     </div>
                     <Button variant="contained" onClick={this.btnClick}>
                         Import
