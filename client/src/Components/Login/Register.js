2f490d8fac84ff0012598bb554079eb38cc75060
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2021-10-13 13:28:57 +0700

    new

--- client/src/Components/Login/Register.js
+++ client/src/Components/Login/Register.js
@@ -32,18 +32,21 @@ class Register extends Component {
     // Send code tới người dùng
     sendCode = (a = this.makeCode(6)) => {
         this.setState({
-            code: a,
+            code: "123456"
         })
-        emailjs.init("user_K1g5N5hUDI0rjsa1uRoI4");
-        emailjs.send("gmail_main", "template_plasdgf", {
-            To_mail: `${document.getElementById('email').value}`,
-            code: `${a}`,
-        })
-            .then((response) => {
-                console.log('SUCCESS!', response.status, response.text);
-            }, (err) => {
-                console.log('FAILED...', err);
-            });
+        // this.setState({
+        //     code: a,
+        // })
+        // emailjs.init("user_K1g5N5hUDI0rjsa1uRoI4");
+        // emailjs.send("gmail_main", "template_plasdgf", {
+        //     To_mail: `${document.getElementById('email').value}`,
+        //     code: `${a}`,
+        // })
+        //     .then((response) => {
+        //         console.log('SUCCESS!', response.status, response.text);
+        //     }, (err) => {
+        //         console.log('FAILED...', err);
+        //     });
     }
 
     // status SignUp 
