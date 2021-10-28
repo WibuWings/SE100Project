18163dcd9ae39e3aeb3783cbb67f271141e8107c
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-10-28 12:05:32 +0700

    getTypeToTab

--- client/src/Components/SellProduct/Tabs.js
+++ client/src/Components/SellProduct/Tabs.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { useSelector , useDispatch} from 'react-redux';
+import { Table } from '@material-ui/core';
 
 export default function IconLabelTabs() {
     const [value, setValue] = React.useState(0);
@@ -15,13 +16,21 @@ export default function IconLabelTabs() {
         if ( newValue === 0 ) {
             index = 'all'
         } else {
-            typeProduct.map((value ,key) => {
+            typeProduct.state.map((value ,key) => {
                  if (key === newValue - 1) {
-                    index = value.type
+                    index = value.name
                 }
             })
         }
+        // console.log(typeProduct.state, typeProduct.state.length);
+        // // typeProduct.map(val => {
+        // //     console.log(val.name);
+        // // });
 
+        // for(var i = 0 ; i < typeProduct.state.length; i++)
+        // {
+        //     console.log("name:", typeProduct.state[i].name)
+        // };
         dispatch({
             type: "UPDATE_TYPE_CHOOSE",
             typeProduct: index,
@@ -36,9 +45,11 @@ export default function IconLabelTabs() {
             aria-label="scrollable auto tabs example"
         >
             <Tab  label="Tất cả" />
-            {typeProduct.map(value => (
-                <Tab  label={value.name} />
-            ))}
+            {typeProduct.state 
+                ?typeProduct.state.map(value => (
+                    <Tab  label={value.name} />))
+                :(null)
+            }
         </Tabs>
     );
 }
