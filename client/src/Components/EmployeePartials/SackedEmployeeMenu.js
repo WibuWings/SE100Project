bb02fc2dc7cca3895600940613829b3101aa38aa
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-11-08 00:23:47 +0700

    back to work success

--- client/src/Components/EmployeePartials/SackedEmployeeMenu.js
+++ client/src/Components/EmployeePartials/SackedEmployeeMenu.js
@@ -15,103 +15,121 @@ class EmployeeMoreMenu extends Component {
   currentEmployee = {};
 
 
-  constructor(props) {
-    super(props);
-    this.state= {
-      change: 'false'
+    constructor(props) {
+        super(props);
+        this.state= {
+        change: 'false'
+        }
+        this.myRef = React.createRef();
     }
-    this.myRef = React.createRef();
-  }
-  setIsOpen(val) {
-    this.isOpen = val;
-    this.setState({change: val});
-    
-  }
-
-  edit() {
-    
-  }
-
-  delete() {
-    const data = {
-      token: localStorage.getItem('token'),
-      employee:
-      [
-          {
-              employeeID: this.props.data,
-              storeID: this.props.infoUser.email, 
-          },
-      ] 
+    setIsOpen(val) {
+        this.isOpen = val;
+        this.setState({change: val});
+        
     }
-    console.log(data);
 
-    axios.delete(`http://localhost:5000/api/employee/delete`,{data: data})
-        .then(res => {
-            alert("delete permantly employee(s) success");
-        })
-        .catch(err => {
-            alert(err);
-        })
-  }
+    edit() {
+        const data = {
+            token: localStorage.getItem('token'),
+            employee:
+            [
+                {
+                    employeeID: this.props.data,
+                    storeID: this.props.infoUser.email,
+                },
+            ]
+            
+        }
+        axios.patch(`http://localhost:5000/api/employee/delete`, data)
+            .then(res => {
+                alert("back to work success");
+            })
+            .catch(err => {
+                alert(err);
+            })
+    }
 
-  isOpen=false;
-  render() {
-    return (
-      <>
-        <IconButton ref={this.myRef} onClick={() => this.setIsOpen(true)}>
-          <Icon icon={moreVerticalFill} width={20} height={20} />
-        </IconButton>
-  
-        <Menu
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-          }}
-          open={this.isOpen}
-          onClose={() => this.setIsOpen(false)}
-          anchorEl={this.myRef.current}
-          // PaperProps={{
-          //   sx: { width: 200, maxWidth: '100%' }
-          // }}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        >
-          <MenuItem sx={{ color: 'text.secondary' }}
-            onClick={() => this.delete()}
-          >
-            <ListItemIcon>
-              <Icon icon={trash2Outline} width={24} height={24} />
-            </ListItemIcon>
-            <ListItemText primary="Delete" primaryTypographyProps={{ variant: 'body2' }} />
-          </MenuItem>
-  
-          <MenuItem sx={{ color: 'text.secondary' }}
-            onClick={() => this.edit()}
-          >
-            <ListItemIcon>
-              <Icon icon={editFill} width={24} height={24} />
-            </ListItemIcon>
-            <ListItemText primary="Edit" primaryTypographyProps={{ variant: 'body2' }} />
-          </MenuItem>
-        </Menu>
-      </>
-    );
-  }
-  
-}
+    delete() {
+        const data = {
+        token: localStorage.getItem('token'),
+        employee:
+        [
+            {
+                employeeID: this.props.data,
+                storeID: this.props.infoUser.email, 
+            },
+        ] 
+        }
+        console.log(data);
+
+        axios.delete(`http://localhost:5000/api/employee/delete`,{data: data})
+            .then(res => {
+                alert("delete permantly employee(s) success");
+            })
+            .catch(err => {
+                alert(err);
+            })
+    }
+
+    isOpen=false;
+    render() {
+        return (
+        <>
+            <IconButton ref={this.myRef} onClick={() => this.setIsOpen(true)}>
+            <Icon icon={moreVerticalFill} width={20} height={20} />
+            </IconButton>
+    
+            <Menu
+            style={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+            }}
+            open={this.isOpen}
+            onClose={() => this.setIsOpen(false)}
+            anchorEl={this.myRef.current}
+            // PaperProps={{
+            //   sx: { width: 200, maxWidth: '100%' }
+            // }}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            >
+            <MenuItem sx={{ color: 'text.secondary' }}
+                onClick={() => this.delete()}
+            >
+                <ListItemIcon>
+                <Icon icon={trash2Outline} width={24} height={24} />
+                </ListItemIcon>
+                <ListItemText primary="Delete" primaryTypographyProps={{ variant: 'body2' }} />
+            </MenuItem>
+    
+            <MenuItem sx={{ color: 'text.secondary' }}
+                onClick={() => this.edit()}
+            >
+                <ListItemIcon>
+                <Icon icon={editFill} width={24} height={24} />
+                </ListItemIcon>
+                <ListItemText primary="Return" primaryTypographyProps={{ variant: 'body2' }} />
+            </MenuItem>
+            </Menu>
+        </>
+        );
+    }
+    
+    }
 const mapStateToProps = (state, ownProps) => {
-  return {
-    updateEmployeeStatus: state.updateEmpoyeeStatus,
-    payEmployeeStatus: state.payEmployeeStatus,
-    listEmployee: state.listEmployee,
-    infoUser: state.infoUser,
-  }
+    return {
+        updateEmployeeStatus: state.updateEmpoyeeStatus,
+        payEmployeeStatus: state.payEmployeeStatus,
+        listEmployee: state.listEmployee,
+        infoUser: state.infoUser,
+    }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-      
-  }
+    return {
+        
+    }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(EmployeeMoreMenu);
