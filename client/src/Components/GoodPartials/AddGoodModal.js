147ecbcf7b1e2ab7e51b035a1264ca6c107e6248
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-12-11 19:39:52 +0700

    add good fix

--- client/src/Components/GoodPartials/AddGoodModal.js
+++ client/src/Components/GoodPartials/AddGoodModal.js
@@ -352,11 +352,11 @@ class AddGoodModal extends Component {
         return(
             <form style={{ zIndex: '10', minWidth: '500px', width: '90%', justifyContent: 'center', marginTop: '10%' }}>   
                 <Card>
-                    <CardHeader style={{ color: 'blue', backgroundColor: '#efeeef' }} title="ADD GOOD" />
+                    <CardHeader style={{ color: !this.props.statusDarkmode? '#0091ea' :'white', backgroundColor: !this.props.statusDarkmode? '#efeeef' :'#455a64'}} title="ADD GOOD" />
                     <Divider />
                     <CardContent>
                         <Grid className="import-container" container spacing={2}>
-                        <Grid item md={3}  
+                        <Grid item md={2}  
                             style={{
                                 display: 'flex', 
                                 justifyContent:'center', 
@@ -366,12 +366,12 @@ class AddGoodModal extends Component {
                             }}
                         >   
                             <label className="profile-header__avatar" for="profile-header-update-avatar" style={{ overflow: 'hidden', marginTop: '15px ' }}>
-                                <Image style={{width: '240px',height: '240px', padding: '10px' }} cloudName="databaseimg" publicId={this.imgUrl=='none' ? 'http://res.cloudinary.com/databaseimg/image/upload/v1634358564/b9wj5lcklxitjglymxqh.png' : this.imgUrl}></Image>
+                                <Image style={{width: '180px',height: '180px', padding: '10px' }} cloudName="databaseimg" publicId={this.imgUrl=='none' ? 'http://res.cloudinary.com/databaseimg/image/upload/v1634358564/b9wj5lcklxitjglymxqh.png' : this.imgUrl}></Image>
                             </label>
                             {/* Ẩn đi */}
                             <input id="profile-header-update-avatar" type="file" style={{ display: 'none' }} accept="image/png, image/jpeg" onChange={(e) => this.profileImageChange(e)}></input>
                         </Grid>
-                        <Grid item md={9}>
+                        <Grid item md={10}>
                                     <Grid container md={12}>
                                         <Grid item md={6} 
                                             className='input-item'
@@ -379,7 +379,7 @@ class AddGoodModal extends Component {
                                             <div 
                                                 className="input-label"
                                                 style={{
-                                                    width: '130px'
+                                                    width: '176px'
                                                 }}
                                             >
                                                 ID
@@ -388,7 +388,7 @@ class AddGoodModal extends Component {
                                                 classname='input-box' 
                                                 type="text" 
                                                 // class="input-val" 
-                                                style = {{width: '60%'}} 
+                                                style = {{width: '100%'}} 
                                                 fullWidth 
                                                 size="small" 
                                                 name="goodID" 
@@ -405,11 +405,11 @@ class AddGoodModal extends Component {
                                                 paddingLeft: 0,
                                             }}
                                         >
-                                            <div className="input-label" style={{width: 110}}>Import Date</div>
+                                            <div className="input-label" style={{width: 138}}>Import Date</div>
                                             <StyledTextField
                                                 classname='input-box'   
                                                 type="date" 
-                                                style = {{width: '68%'}} 
+                                                style = {{width: '100%'}} 
                                                 fullWidth
                                                 name="importDate"
                                                 size="small"
@@ -421,12 +421,12 @@ class AddGoodModal extends Component {
                                         <Grid item md={6} 
                                             className='input-item'
                                         >
-                                            <div className="input-label"style={{width: '130px'}}>Name</div>
+                                            <div className="input-label"style={{width: '176px'}}>Name</div>
                                             <StyledTextField
                                                 classname='input-box'   
                                                 type="text" 
                                                 // class="input-val" 
-                                                style = {{width: '60%'}} 
+                                                style = {{width: '100%'}} 
                                                 fullWidth
                                                 size="small"
                                                 name="goodName" 
@@ -435,11 +435,11 @@ class AddGoodModal extends Component {
                                         </Grid>
                                         <Grid item md={4}
                                             className='input-item'
-                                            style={{padding: '0px', marginLeft: '0px'}}
+                                            style={{padding: '0px', marginLeft: '0px', marginRight: 0}}
                                         >
                                             <div 
                                                 className="input-label" 
-                                                style={{width: '120px'}}
+                                                style={{width: '110px'}}
                                             >
                                                 Quantity
                                             </div>
@@ -454,14 +454,14 @@ class AddGoodModal extends Component {
                                         <Grid item md={2}
                                             className='input-item'
                                             style={{
-                                                paddingRight: 12
+                                                paddingRight: 5
                                             }}
                                         >
                                             <div 
                                                 className="input-label"
                                                 style={{
                                                     marginLeft: 0,
-                                                    paddingLeft: 4,
+                                                    paddingLeft: 0,
                                                     width: 40
                                                 }}
                                             >
@@ -479,11 +479,11 @@ class AddGoodModal extends Component {
                                         <Grid item md={6} 
                                             className='input-item'
                                         >
-                                            <div className="input-label" style={{width: 132}}>Expired Date</div>
+                                            <div className="input-label" style={{width: 176}}>Expired Date</div>
                                             <StyledTextField
                                                 classname='input-box'   
                                                 type="date" 
-                                                style = {{width: '70%'}} 
+                                                style = {{width: '100%'}} 
                                                 fullWidth
                                                 size="small"
                                                 name="expiredDate" 
@@ -491,14 +491,14 @@ class AddGoodModal extends Component {
                                                 defaultValue={this.currentDateTime}
                                             />
                                         </Grid>
-                                        <Grid item md={6} className='input-item'>
-                                            <div className="input-label" style={{width: 100}}>Currency</div>
+                                        <Grid item md={6} className='input-item' style={{marginLeft: -8}}>
+                                            <div className="input-label" style={{width: 138}}>Currency</div>
                                             <StyledTextField
                                                 // fullWidth
                                                 id="currencySelector"
                                                 name="currency"
                                                 variant="outlined"
-                                                style = {{width: '70%'}} 
+                                                style = {{width: '100%'}} 
                                                 select
 
                                                 SelectProps={{ native: true }}
@@ -523,16 +523,19 @@ class AddGoodModal extends Component {
                                         <Grid item md={6}
                                             className='input-item'
                                         >
-                                            <div className="input-label" style={{width: 128}}>Original Price</div>
+                                            <div className="input-label" style={{width: 189}}>Original Price</div>
                                             <StyledTextField
                                                 classname='input-box'
-                                                style = {{width: '60%', marginLeft: '4px', marginRight:'8px'}} 
+                                                style = {{width: '100%', marginRight: 8}} 
                                                 fullWidth
                                                 name="originalPrice" 
                                                 variant="outlined"
                                                 type="number" 
                                             />
-                                            { this.curCurrencySelect == 'vnd' ? <div>VNĐ</div> : <div>$</div>}
+                                            <div style={{width: 30}}>
+                                                { this.curCurrencySelect == 'vnd' ? <div>VNĐ</div> : <div>$</div>}
+                                            </div>
+                                            
                                         </Grid>
                                         <Grid item md={6}
                                             className='input-item'
@@ -540,13 +543,13 @@ class AddGoodModal extends Component {
                                         >
                                             <div 
                                                 className="input-label"
-                                                style={{width: '130px'}}
+                                                style={{width: '150px'}}
                                             >
                                                 Sell Price
                                             </div>
                                             <StyledTextField
                                                 classname='input-box'
-                                                style = {{width: '80%', marginLeft: '4px',marginRight:'8px'}} 
+                                                style = {{width: '100%', marginLeft: '4px',marginRight:'8px'}} 
                                                 fullWidth
                                                 name="sellPrice" 
                                                 variant="outlined"
@@ -560,7 +563,7 @@ class AddGoodModal extends Component {
                                         >
                                             <div 
                                                 className="input-label"
-                                                style={{width:132}}
+                                                style={{width:129}}
                                             >
                                                 Product Type
                                             </div>
@@ -673,6 +676,7 @@ const mapStateToProps = (state, ownProps) => {
         typeProduct: state.typeProduct,
         listProduct: state.listProduct,
         regulation: state.regulationReducer,
+        statusDarkmode: state.statusDarkmode,
     }
 }
 
